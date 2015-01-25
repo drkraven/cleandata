b751033b1c0b9d16d20f02907e9297793f31077b
H Lan <dpbluevoid@yahoo.com>
2015-01-25 17:42:23 -0500

    added comments

--- cleandata_project.R
+++ cleandata_project.R
@@ -10,15 +10,18 @@ library(reshape2)
 
 ##read Feature labels
 featureLabels <- read.table("/home/developer/courses/cleandata/uci_ds/features.txt")
+
+##read activity labels
 activityLabels <- read.table("/home/developer/courses/cleandata/uci_ds/activity_labels.txt")
 
-mutateActivityLabel <- function(filePath) {
-  
+## read in Y values and substitue values with the corresponding activity labels
+mutateActivityLabel <- function(filePath) {  
   yValue  <- read.table(filePath)
   y <- mutate(yValue, act_label= activityLabels[yValue$V1, 2] )
   return(y[,2])
 }
 
+## read in observed measures and extract only mean and standard deviation variables for each measure
 extractMeasures <- function(filePath) {
   measures <- read.table(filePath)
   colnames(measures) <- featureLabels[,2]
@@ -26,6 +29,7 @@ extractMeasures <- function(filePath) {
   return(retMeasures)
 }
 
+## create a dataset with observed measures with correstponding subject_id annd activities
 assembleDS <- function(subjectPath, activityPath, featuresPath) {
   subjectDS <- read.table(subjectPath)
   activityDS <- mutateActivityLabel(activityPath)
@@ -39,14 +43,18 @@ assembleDS <- function(subjectPath, activityPath, featuresPath) {
   return(ds)
 }
 
-
+# read and create training dataset
 trainingDS <-  assembleDS("/home/developer/courses/cleandata/uci_ds/train/subject_train.txt", "/home/developer/courses/cleandata/uci_ds/train/y_train.txt", "/home/developer/courses/cleandata/uci_ds/train/X_train.txt")
+# read and create test dataset
 testDS <- assembleDS("/home/developer/courses/cleandata/uci_ds/test/subject_test.txt", "/home/developer/courses/cleandata/uci_ds/test/y_test.txt", "/home/developer/courses/cleandata/uci_ds/test/X_test.txt")
 
-
+#merge test and training dataset into one
 mergedDS <- rbind(trainingDS, testDS)
+
+#create a new tidy dataset that contains subject_id, actitvity, and the average value of each variable
 mergedDS_melt <- melt(mergedDS, id=c("subject_id","activity_label"))
 ds_mean <- dcast(mergedDS_melt, subject_id+activity_label ~ variable, mean)
-
+# order new dataset by subject_id
 ds_mean <- arrange(ds_mean, subject_id)
-write.table(ds_mean, file = "clean_set.txt", row.names = FALSE)
+# generate output
+write.table(ds_mean, file = "clean_solution_hl.txt", row.names = FALSE)
